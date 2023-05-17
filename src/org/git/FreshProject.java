b30050feb91f00ef6881c1fe1d9417fada220c41
Rathnasujay <rathnasujayuidesigner@gmail.com>
2023-05-17 10:58:48 +0530

    code pushed

---
+++ src/org/git/FreshProject.java
@@ -0,0 +1,10 @@
+package org.git;
+
+public class FreshProject {
+public static void main(String[] args) {
+	System.out.println(12);
+	System.out.println(12);
+	System.out.println(12);
+	System.out.println(12);
+}
+}
